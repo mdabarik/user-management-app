c814ade8ea74382d4e895f1e853c87ec6aee7729
mdabarik <mdabarik19@gmail.com>
2023-10-17 11:06:06 +0600

    router added

---
+++ src/Router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Router</h1>
+    }
+])
+export default router;
